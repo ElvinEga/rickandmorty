03ea4ea94def8c9ac028141defc6842fbba7160c
elvinega <proega3@gmail.com>
2024-02-16 17:50:36 +0300

    fix issues

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -34,91 +34,80 @@ export default function CharacterPage() {
         <main className="main-wrapper relative overflow-hidden">
           <>
             {/* Start Hero */}
-            {location ? (
-              <>
-                <section className="relative table w-full py-32 lg:py-40 bg-[url('/assets/img/header.jpg')] bg-cover bg-center">
-                  <div className="container">
-                    <div className="grid grid-cols-1 text-center mt-10">
-                      <h3 className="text-6xl leading-normal font-bold">
-                        Characters
-                      </h3>
-                    </div>
+            <>
+              <section className="relative table w-full py-32 lg:py-40 bg-[url('/assets/img/header.jpg')] bg-cover bg-center">
+                <div className="container">
+                  <div className="grid grid-cols-1 text-center mt-10">
+                    <h3 className="text-6xl leading-normal font-bold">
+                      Characters
+                    </h3>
                   </div>
-                </section>
-                <section className="relative md:py-24 py-16">
-                  <div className="container">
-                    <div className="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
-                      {characters.map((character) => (
-                        <Link
-                          className="group rounded-xl overflow-hidden dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                          href={`/character?id=${character.id}`}
-                        >
-                          <div className="sm:flex">
-                            <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-56 sm:w-56 h-56">
-                              <img
-                                className="group-hover:scale-125 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
-                                src={character.image}
-                                alt="Image Description"
-                              />
-                            </div>
-                            <div className="grow sm:mt-0 sm:ms-6 px-4 sm:px-0">
-                              <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
-                                {character.name}
-                              </h3>
-                              <p>
-                                {character.status} - {character.species}
-                              </p>
-                              <p className="mt-3 text-gray-600 dark:text-gray-400">
-                                Location
-                              </p>
-                              <p className=" text-lg text-gray-800 dark:text-gray-400">
-                                {character.location.name}
-                              </p>
-                              <p className="mt-3 text-gray-600 dark:text-gray-400">
-                                Gender
-                              </p>
-                              <p className=" text-lg text-gray-800 dark:text-gray-400">
-                                {character.gender}
-                              </p>
-                              <Link
-                                href={`/character?id=${character.id}`}
-                                className="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium"
-                              >
-                                Read more
-                                <svg
-                                  className="flex-shrink-0 size-4"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width={24}
-                                  height={24}
-                                  viewBox="0 0 24 24"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  strokeWidth={2}
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                >
-                                  <path d="m9 18 6-6-6-6" />
-                                </svg>
-                              </Link>
-                            </div>
-                          </div>
-                        </Link>
-                      ))}
-                    </div>
-                  </div>
-                </section>
-              </>
-            ) : (
-              <div className="flex justify-center">
-                <div
-                  className="animate-spin inline-block h-16 w-16 m-44 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
-                  role="status"
-                  aria-label="loading"
-                >
-                  <span className="sr-only">Loading...</span>
                 </div>
-              </div>
-            )}
+              </section>
+              <section className="relative md:py-24 py-16">
+                <div className="container">
+                  <div className="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
+                    {characters.map((character) => (
+                      <Link
+                        className="group rounded-xl overflow-hidden dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        href={`/character?id=${character.id}`}
+                      >
+                        <div className="sm:flex">
+                          <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-56 sm:w-56 h-56">
+                            <img
+                              className="group-hover:scale-125 transition-transform duration-500 ease-in-out size-full absolute top-0 start-0 object-cover rounded-xl"
+                              src={character.image}
+                              alt="Image Description"
+                            />
+                          </div>
+                          <div className="grow sm:mt-0 sm:ms-6 px-4 sm:px-0">
+                            <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
+                              {character.name}
+                            </h3>
+                            <p>
+                              {character.status} - {character.species}
+                            </p>
+                            <p className="mt-3 text-gray-600 dark:text-gray-400">
+                              Location
+                            </p>
+                            <p className=" text-lg text-gray-800 dark:text-gray-400">
+                              {character.location.name}
+                            </p>
+                            <p className="mt-3 text-gray-600 dark:text-gray-400">
+                              Gender
+                            </p>
+                            <p className=" text-lg text-gray-800 dark:text-gray-400">
+                              {character.gender}
+                            </p>
+                            <Link
+                              href={`/character?id=${character.id}`}
+                              className="mt-4 inline-flex items-center gap-x-1 text-blue-600 decoration-2 hover:underline font-medium"
+                            >
+                              Read more
+                              <svg
+                                className="flex-shrink-0 size-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={24}
+                                height={24}
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              >
+                                <path d="m9 18 6-6-6-6" />
+                              </svg>
+                            </Link>
+                          </div>
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </section>
+            </>
+
             {/* End */}
           </>
         </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,8 @@
 
 import "./globals.css";
 import { useEffect } from "react";
-import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
-export const metadata: Metadata = {
-  title: "Rick & Morty Universe",
-  description: "Explore the characters, locations, and lore of Rick and Morty",
-};
 
 export default function RootLayout({
   children,
